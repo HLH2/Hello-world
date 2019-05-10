8e4562538ae98908c419339361b2c62aee631d69
HLH2 <46175758+HLH2@users.noreply.github.com>
2019-05-10 11:10:21 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,28 @@
+class helh{
+    public static void main(String[] arge) {
+        System.out.println("I like old frenid");
+        System.out.println("This is my test-01");
+        System.out.println("Oh,my english is very poor");
+    }
+}
+
+class nihaoa {
+    public static void main(String[] arge) {
+        System.out.println("Hello World");
+
+    }
+}
+
+
+class WoXiangNiL
+{
+    public static void main(String[] arge){
+
+        int myInt = 9;
+        System.out.println("int");
+
+
+        byte b1 = 126;
+        short s1 = 117;
+    }
+}
