5591e68cb80702830d4e9db87528737041747c83
longhui he <46175758+HLH2@users.noreply.github.com>
2019-06-20 14:42:49 +0800

    Delete Test_JAVA_MyTest.java

--- Test_JAVA_MyTest.java
+++
@@ -1,8 +0,0 @@
-public class Test_JAVA_MyTest {
-    public static void main(String[] args){
-        int x = 20;
-        int t = 8;
-        int over = x % t;
-        System.out.println(over);
-    }
-}
