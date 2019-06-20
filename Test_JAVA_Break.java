0c995a31e591fef1b924b66c7171d7019dc15142
longhui he <46175758+HLH2@users.noreply.github.com>
2019-06-20 14:38:31 +0800

    Add files via upload

---
+++ Test_JAVA_Break.java
@@ -0,0 +1,10 @@
+public class Test_JAVA_Break {
+    public static void main(String[] args) {
+        for (int x = 1;x <= 10 ;x++) {
+            if (x == 4){
+                break;
+            }
+            System.out.println("x = " + x);
+        }
+    }
+}
---
+++ Test_JAVA_MyTest.java
@@ -0,0 +1,8 @@
+public class Test_JAVA_MyTest {
+    public static void main(String[] args){
+        int x = 20;
+        int t = 8;
+        int over = x % t;
+        System.out.println(over);
+    }
+}
