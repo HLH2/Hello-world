e3196c88d12d9450a208fb0bbacae2fa03d6255b
longhui he <46175758+HLH2@users.noreply.github.com>
2019-06-20 14:43:24 +0800

    Delete Test_JAVA_While.java

--- Test_JAVA_While.java
+++
@@ -1,9 +0,0 @@
-class Test_JAVA_While {
-        public static void main(String[] args) {
-            int i = 1;
-            while (i <= 2 ) {
-            System.out.println("I LOVE YOU Three times" +i );
-                i++;
-        }
-    }
-}
