45fde5f1b289b727c03759e13d95758e15e68e0b
longhui he <46175758+HLH2@users.noreply.github.com>
2019-07-22 14:27:38 +0800

    Update Dome1_Abstract.java

--- 面向对象-多态接口/Dome1_Abstract.java
+++ 面向对象-多态接口/Dome1_Abstract.java
@@ -1,32 +1,32 @@
 public class Dome1_Abstract {
 	public static void main(String[] args) {
-		Animal a = new Cat();     //ʹ�ø��������������
-        a.eat();
+		Animal a = new Cat();     //使用父类引用子类对象
+        	a.eat();
 	}
 }
 
 /*
-��������ص㣺
-    1.������ͳ��󷽷�����ʹ��abstract �ؼ���ʹ��
-        ��ĸ�ʽΪ��abstract class ���� {} 
-        �����ĸ�ʽΪ��public abstract void main();
-    2.�����಻һ���г��󷽷����г��󷽷�����һ���ǳ���������ǽ���
-    3.�����಻��ʵ������ô������ʹ�ö�̨�ķ�ʽ���о��������ʵ��������ʵ��Ҳ�Ƕ�̬��һ�֣��������̬
-    4.��������ࣺ
-        Ҫô�ǳ�����
-        Ҫô��д�������е����г��󷽷���
+抽象类的特点：
+    1.抽象类和抽象方法必须使用abstract 关键字使用
+        类的格式为：abstract class 类名 {} 
+        方法的格式为：public abstract void main();
+    2.抽象类不一定有抽象方法，有抽象方法的类一定是抽象类或者是解耦
+    3.抽象类不能实例化那么，可以使用多台的方式，有具体的子类实例化，其实这也是多态的一种，抽象类多态
+    4.抽象的子类：
+        要么是抽象类
+        要么重写抽象类中的所有抽象方法‘
         
-�������еĳ�Ա�������ԣ�
-    1.���󷽷���ǿ��Ҫ��������������
-    2.�ǳ��󷽷�������̳е����飬��ߴ���ĸ�����
+抽象类中的成员方法特性：
+    1.抽象方法，强制要求子类做的事情
+    2.非抽象方法，子类继承的事情，提高代码的复用性
 */
 
-abstract class Animal {      //����һ��������
-    public abstract void eat();     //����һ�����󷽷�
+abstract class Animal {      //这是一个抽象类
+    public abstract void eat();     //这是一个抽象方法
 }
 
 class Cat extends Animal {
-    public void eat() {         //ʹ���ӷ����̳г��󷽷��󣬳��󷽷�������г����඼��Ҫ����д
-        System.out.println("è����");
+    public void eat() {         //使用子方法继承抽象方法后，抽象方法里的所有抽象类都需要被重写
+        System.out.println("猫吃鱼");
     }
 }
